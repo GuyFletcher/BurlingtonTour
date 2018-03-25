1b8ad282d75e538b0ac23c5dc529cf4be59ced9c
Hart <fletcherhart@gmail.com>
2018-03-25 12:35:30 -0400

    first commit

---
+++ burlington/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  burlington
+//
+//  Created by Hart, Fletcher on 3/25/18.
+//  Copyright © 2018 Hart, Fletcher. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
